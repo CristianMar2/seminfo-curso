a438ca9349f63826ec64de2141025db573f495b8
CristianMar2 <cristianmarfer14@gmail.com>
2024-10-31 10:00:12 -0300

    Adicionar barra de navegação

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,10 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class HomeModule { }
